93211f78f2533e14c5d609618cdbac5293976815
김태훈 <gimtaehun@gimtaehun-ui-MacBookPro.local>
2020-05-01 16:53:40 +0900

    2주차/과제

--- 2주차/SecondWeek/SecondWeek/RegisterViewController.swift
+++ 2주차/SecondWeek/SecondWeek/RegisterViewController.swift
@@ -9,18 +9,30 @@
 import UIKit
 
 class RegisterViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var giveInfo: UILabel!
     @IBOutlet weak var registerName: UITextField!
     @IBOutlet weak var registerEmail: UITextField!
     @IBOutlet weak var registerPW: UITextField!
     
+    @IBOutlet weak var checkbutoon: UIButton!
     let mail = "st939823@gmail.com"
     @IBOutlet weak var RegisterButton: UIButton!
-    @IBAction func registerButtonClick(_ sender: Any) {
-        var message = "사용 가능한 이메일입니다."
+    @IBAction func checkEmail(_ sender: Any) {
+        var message:String?
         if (self.registerEmail.isEqual(self.mail)){
             message = "중복된 이메일입니다."
+            checkbutoon.setImage(UIImage(named: "idcheckRedIc"), for: .normal)
         }
-        else{self.navigationController?.popViewController(animated: true)
+        else{
+            checkbutoon.setImage(UIImage(named: "idcheckGreenIc"), for: .normal)
+            message = "사용 가능한 이메일입니다."
+        }
+        giveInfo.text = message
+    }
+    
+    @IBAction func registerButtonClick(_ sender: Any) {
+        if (!self.registerEmail.isEqual(self.mail)){
+            self.navigationController?.popViewController(animated: true)
         }
         
     }
@@ -28,15 +40,20 @@ class RegisterViewController: UIViewController, UITextFieldDelegate{
         self.navigationController?.popViewController(animated: true)
     }
     func layout(){
+        let redius:CGFloat = 22.0
         RegisterButton.layer.cornerRadius=24
-        self.registerName.placeholder="이름"
-        self.registerEmail.placeholder="이메일"
-        self.registerPW.placeholder="비밀번호"
-        registerEmail.layer.cornerRadius = 22
-        registerPW.layer.cornerRadius=22
-        registerName.layer.cornerRadius=22
+        self.giveInfo.layer.cornerRadius=redius
+        self.registerName.placeholder="  이름"
+        self.registerEmail.placeholder="  이메일"
+        self.registerPW.placeholder="  비밀번호"
+        registerEmail.layer.cornerRadius = redius
+        registerPW.layer.cornerRadius=redius
+        registerName.layer.cornerRadius=redius
+        self.registerName.layer.masksToBounds=true
         self.registerEmail.delegate = self
         self.registerPW.delegate = self
+        self.registerEmail.layer.masksToBounds=true
+        self.registerPW.layer.masksToBounds=true
         self.registerPW.isSecureTextEntry=true
     }
     override func viewDidLoad() {
---
+++ 2주차/SecondWeek_calculator/SecondWeek_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondWeek_calculator
+//
+//  Created by 김태훈 on 2020/05/01.
+//  Copyright © 2020 김태훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
